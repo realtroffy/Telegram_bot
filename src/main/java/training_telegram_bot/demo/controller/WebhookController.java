c08e7c2126889f0e0dfeb6b86ca0f15350dd56d4
realtroffy <artur.asiptsov@yandex.ru>
2022-08-26 20:01:52 +0300

    add csrf

--- src/main/java/training_telegram_bot/demo/controller/WebhookController.java
+++ src/main/java/training_telegram_bot/demo/controller/WebhookController.java
@@ -12,14 +12,14 @@ import training_telegram_bot.demo.model.UserWriteBot;
 import training_telegram_bot.demo.service.TelegramFacade;
 import training_telegram_bot.demo.service.WebHookService;
 
-@RestController("/update")
+@RestController
 @AllArgsConstructor
 public class WebhookController {
 
   private final WebHookService webHookService;
   private final TelegramFacade telegramFacade;
 
-  @PostMapping
+  @PostMapping("/update")
   public Message onUpdateReceived(@RequestBody Update update) {
     UserWriteBot userWriteBot = webHookService.getUserFromUpdate(update);
     webHookService.saveToDataBase(userWriteBot);
