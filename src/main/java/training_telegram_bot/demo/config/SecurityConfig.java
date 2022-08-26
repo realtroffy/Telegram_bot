e5b32239622c31fa48ae31661a0f5a30a37dd43f
realtroffy <artur.asiptsov@yandex.ru>
2022-08-26 21:56:29 +0300

    add health page

--- src/main/java/training_telegram_bot/demo/config/SecurityConfig.java
+++ src/main/java/training_telegram_bot/demo/config/SecurityConfig.java
@@ -32,10 +32,7 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    http.csrf()
-        .ignoringAntMatchers("/update")
-        .and()
-        .authorizeHttpRequests()
+    http.authorizeHttpRequests()
         .antMatchers("/health", "/update", "/login", "/error")
         .permitAll()
         .anyRequest()
